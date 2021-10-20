e2996015a8cf56e2cbda2efc131e38c5b6e93465
Robert Isaacs <robertisaacs87@yahoo.com>
2021-10-19 21:34:58 -0500

    setting up functions for index.js

--- Develop/index.js
+++ Develop/index.js
@@ -1,14 +1,74 @@
 // TODO: Include packages needed for this application
+const fs require("fs");
 const inquirer = require('inquirer');
-console.log(inquirer);
+const generateMarkdown = require("./utils/generateMarkdown");
+// License function and if/else section
+function getLicense(value) {
+    if (value === "GNU AGPLv3") {
+        return "[![License: AGPL v3](https://img.shields.io/badge/License-AGPL%20v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)";
+    } else if (value === "GNU GPLv3") {
+        return "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)";
+    } else if (value === "GNU LGPLv3") {
+        return "[![License: LGPL v3](https://img.shields.io/badge/License-LGPL%20v3-blue.svg)](https://www.gnu.org/licenses/lgpl-3.0)";
+    } else if (value === "Apache 2.0") {
+        return "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
+    } else if (value === "Boost Software 1.0") {
+        return "[![License](https://img.shields.io/badge/License-Boost%201.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)";
+    } else if (value === "MIT") {
+        return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+    } else {
+        return "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)";
+    }
+}
+
+function validateInput(value) {
+    if (value != "") {
+        return true;
+    } else {
+        return "Please answer with some kind of input.";
+    }
+}
+
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    // Question for Title
+
+    //Question for the project Description
+
+    //Table of Contents, handling this in the markdown.js
+
+    // Question for Installation
+
+    //Question for Usage
+
+    //Question for License
+
+    //Question for Contributing
+
+    //Question for Tests
+
+    //QUESTIONS section -- github
+
+    //QUESTIONS section -- email address
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeToFile(fileName, generateMarkdown(data), function (err) {
+        if (err) {
+            return console.log(err);
+        }
+    });
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then((date) => {
+        console.log(JSON.stringify(data, null, ""));
+        data.getLicense = getLicense(data.license);
+        writeToFile("./example/README.md", data);
+    });
+}
 
 // Function call to initialize app
 init();
